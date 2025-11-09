f6cd6615a8f973cf87a7abe6eecfe25c2f96972f
Abhishek Prajapati <bhishekprajapati@gmail.com>
2025-11-09 14:56:53 +0530

    feat: add neovim, fzf, curl to setup.sh

--- executable_setup.sh
+++ executable_setup.sh
@@ -1,4 +1,67 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+is_root() {
+  if [[ $(id -u) -eq 0 ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+maybe_sudo() {
+  if ! is_root; then
+    sudo "$@"
+  else
+    "$@"
+  fi
+}
+
+str_trim() {
+  echo "$*" | xargs
+}
+
+str_to_lowercase() {
+  echo "$*" | tr '[:upper:]' '[:lower:]'
+}
+
+str_is_empty() {
+  local -r input="$*"
+
+  if [[ -z "$input" ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+readonly OS="$(uname -s)"
+readonly ARCH="$(uname -m)"
+readonly NORMALIZED_OS="$(str_trim "$(str_to_lowercase "$OS")")"
+readonly NORMALIZED_ARCH="$(str_trim "$(str_to_lowercase "$ARCH")")"
+has_updated_apt=false
+
+update_apt() {
+  if [[ $has_updated_apt == false ]]; then
+    if maybe_sudo apt update; then
+      has_updated_apt=true
+    fi
+  fi
+}
+
+is_exec() {
+  local -r name="$1"
+  command -v "$name" >/dev/null 2>&1
+}
+
+os_is_linux() {
+  if [[ "$NORMALIZED_OS" == "linux" ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
 
 color() {
   local -r COLOR_RESET="\033[0m"
@@ -69,9 +132,13 @@ log() {
     tag=$(color -cyan "info")
     rest_args=$(color -cyan "${@:2}")
     ;;
+  -s)
+    tag=$(color -green "info")
+    rest_args=$(color -green "${@:2}")
+    ;;
   *)
-    tag=$(color -cyan "info")
-    rest_args=$(color -cyan "$@")
+    tag=$(color -white "info")
+    rest_args=$(color -white "$@")
     ;;
   esac
 
@@ -95,12 +162,99 @@ $(printf '%0.s#' {1..81})
 EOF
   )")
 
-  local -r colored=$(color -cyan "${text[*]}")
+  local -r colored=$(color -blue "${text[*]}")
   echo -e "${colored[*]}"
 }
 
+install() {
+  get_installer_name() {
+    local -r bin_name="$1"
+    echo "install_${bin_name}_${NORMALIZED_OS}_${NORMALIZED_ARCH}"
+  }
+
+  has_installer() {
+    local -r func_name="$1"
+    declare -F "$func_name" >/dev/null
+  }
+
+  install_neovim_linux_x86_64() {
+    if is_exec "nvim"; then
+      log -i "[skipped]: neovim is already installed."
+      return 0
+    fi
+
+    local -r filename="nvim-linux-x86_64.tar.gz"
+
+    cleanup() {
+      rm -f "$filename" || true
+    }
+
+    cleanup
+
+    {
+      curl -fLO "https://github.com/neovim/neovim/releases/download/v0.11.2/$filename" &&
+        maybe_sudo tar -C "/opt" -xzf "$filename" &&
+        rm -rf "/opt/nvim-linux-x86_64/.git" &&
+        log -s "Neovim installed!"
+    } || {
+      log -e "Failed to install neovim"
+    }
+
+    cleanup
+  }
+
+  local -r bin_name="$1"
+  local -r installer_name="$(get_installer_name "$bin_name")"
+
+  if has_installer "$installer_name"; then
+    "$installer_name"
+  else
+    log -e "No installer implemented"
+  fi
+}
+
 main() {
-  banner
+  banner 2>/dev/null || true
+
+  log "Checking operating system..."
+  log "Detected $NORMALIZED_OS os"
+  log "Detected architecture $NORMALIZED_ARCH"
+
+  if ! os_is_linux; then
+    log -w "Not implemented for this os"
+    return 0
+  fi
+
+  install_base_tools() {
+    if ! is_exec 'apt'; then
+      log -e "Can't find apt package manager. Failed to install fuzzy finder."
+      return 1
+    fi
+
+    update_apt
+
+    local -r tools=("fzf" "curl")
+    log -i "Installing base tools ${tools[*]}"
+
+    local tool=''
+    for tool in "${tools[@]}"; do
+      if is_exec "$tool"; then
+        log -i "[Skipped]: $tool is already installed."
+      else
+        apt install -y "$tool"
+      fi
+    done
+  }
+
+  install_base_tools
+
+  local -r bins=("neovim" "go" "rust" "ripgrep" "lazygit")
+  selected=$(printf "%s\n" "${bins[@]}" | fzf --multi --prompt="Select binaries> ")
+
+  local bin_name=""
+  for bin_name in $selected; do
+    install "$bin_name"
+  done
 }
 
 main
